59520a643cea317b53c4d330b3553bb6ba038ede
Lukasz Jasinski <jasinski-lukasz@hotmail.com>
2020-11-17 23:44:54 +0100

    add admin controller + add admin index and basic form +add style for product list

---
+++ MilenaEmbroidery.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilenaEmbroidery.DTOs.Shop;
+using MilenaEmbroidery.IServices.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilenaEmbroidery.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<ProductDTO> products = Enumerable.Empty<ProductDTO>();
+
+            try
+            {
+                products = await _productService.Get();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.GetBaseException().Message);
+            }
+
+            return View(products);
+        }
+
+        public IActionResult BasicForm()
+        {
+            return View();
+        }
+    }
+}
